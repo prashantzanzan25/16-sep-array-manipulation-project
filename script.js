0a1946edb178ca6ec5ba06ceaf2ae2cad6693af4
Prashant Zanzan <139844406+prashantzanzan25@users.noreply.github.com>
2023-09-16 18:21:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,14 +4,14 @@ let peopleArray = [
   { name: "jane", age: 27, profession: "admin" },
 ];
 
-// Task 1: Print Developers
+// 1: Print Developers
 function printDevelopers() {
   for (let person of peopleArray) {
     console.log(person.name, '-', person.profession);
   }
 }
 
-// Task 2: Add Data
+// 2: Add Data
 function addData() {
   const name = prompt('Enter name:');
   const age = parseInt(prompt('Enter age:'));
@@ -21,12 +21,12 @@ function addData() {
   peopleArray.push(newData);
 }
 
-// Task 3: Remove Admins
+// 3: Remove Admins
 function removeAdmin() {
   peopleArray = peopleArray.filter(person => person.profession !== 'admin');
 }
 
-// Task 4: Concatenate Array
+// 4: Concatenate Array
 function concatenateArray() {
   const dummyArray = [
     { name: 'Sam', age: 35, profession: 'developer' },
@@ -37,32 +37,32 @@ function concatenateArray() {
   console.log(combinedArray);
 }
 
-// Task 5: Average Age
+// 5: Average Age
 function averageAge() {
   const totalAge = peopleArray.reduce((sum, person) => sum + person.age, 0);
   const avgAge = totalAge / peopleArray.length;
   console.log('Average Age:', avgAge.toFixed(2));
 }
 
-// Task 6: Age Check
+// 6: Age Check
 function checkAgeAbove25() {
   const isAbove25 = peopleArray.some(person => person.age > 25);
   console.log('Is there at least one person above 25?', isAbove25);
 }
 
-// Task 7: Unique Professions
+// 7: Unique Professions
 function uniqueProfessions() {
   const professions = [...new Set(peopleArray.map(person => person.profession))];
   console.log('Unique Professions:', professions.join(', '));
 }
 
-// Task 8: Sort by Age
+// 8: Sort by Age
 function sortByAge() {
   peopleArray.sort((a, b) => a.age - b.age);
   console.log('Sorted by Age:', peopleArray);
 }
 
-// Task 9: Update Profession
+// 9: Update Profession
 function updateJohnsProfession() {
   const john = peopleArray.find(person => person.name === 'John');
   if (john) {
@@ -70,7 +70,7 @@ function updateJohnsProfession() {
   }
 }
 
-// Task 10: Profession Count
+// 10: Profession Count
 function getTotalProfessions() {
   const developerCount = peopleArray.filter(person => person.profession === 'developer').length;
   const adminCount = peopleArray.filter(person => person.profession === 'admin').length;
@@ -78,14 +78,6 @@ function getTotalProfessions() {
   console.log('Total Admins:', adminCount);
 }
 
-// Test the functions
-// printDevelopers();
-// addData();
-// removeAdmin();
-// concatenateArray();
-// averageAge();
-// checkAgeAbove25();
-// uniqueProfessions();
-// sortByAge();
-// updateJohnsProfession();
-// getTotalProfessions();
+
+
+
